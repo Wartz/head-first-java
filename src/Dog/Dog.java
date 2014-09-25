ac719c567757c41f4522330b43748476dfe899c1
Joe Schlimmer <wartzay@gmail.com>
2014-09-25 08:26:26 -0400

    reorganization of folder structure

---
+++ src/Dog/Dog.java
@@ -0,0 +1,20 @@
+package Dog;
+
+public class Dog {
+    public static void main (String[] args) {
+        System.out.print("woof!\n");
+        int x =3;
+        String name = "Dirk";
+        x=x*17;
+        System.out.print("x is " + x);
+        double d = Math.random();
+        //this is a comment
+        
+        while (x>12) {
+        x=x-1;
+        }
+        for (int b=0; b<10; b=x+1) {
+        System.out.print("X is now "+b);
+        }
+    }
+}
---
+++ src/Party/Party.java
@@ -0,0 +1,15 @@
+package Party;
+
+import java.awt.*;
+import java.awt.event.*;
+
+class Party {
+    public void buildInvite (){
+        Frame f = new Frame();
+        Label l = new Label("Party at Tim's");
+        Button b = new Button("You Bet");
+        Button c = new Button("Shoot me");
+        Panel p = new Panel();
+        p.add(l);
+    }
+}
