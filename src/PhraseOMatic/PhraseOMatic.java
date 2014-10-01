9cf54899117954e0cb9e710503e999586d2f3837
Joe Schlimmer <wartzay@gmail.com>
2014-10-01 18:52:16 -0400

    added phrashomatic and shuffle applications

---
+++ src/PhraseOMatic/PhraseOMatic.java
@@ -0,0 +1,21 @@
+public class PhraseOMatic {
+    public static void main (String[] args) {
+        //three sets of words
+        String[] wordListOne = {"bring it on", "Tier","30,000 foot","B-to-B","win-win","frontend", "web-based","pervasive", "smart", "sixsigma"," critical-path", "dynamic"};
+        String[] wordListTwo = {"empowered", "sticky", "value-added", "oriented", "centric", "distributed", "clustered", "branded","outside-the-box", "positioned", "networked", "focused", "leveraged", "aligned", "targeted", "shared", "cooperative", "accelerated"};
+        String[] wordListThree = {"process", "tippingpoint", "solution", "architecture", "core competency", "strategy", "mindshare", "portal", "space", "vision", "paradigm", "mission"};
+        
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+        
+        //random pairs
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+        
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+        
+        System.out.println("What we need is a " + phrase + ".");
+    }
+}
