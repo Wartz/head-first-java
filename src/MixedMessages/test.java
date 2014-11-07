db6c59f31d1a16f8f4c94b5bd007d9916a74ff54
Wartz <wartzay@gmail.com>
2014-11-07 02:08:40 -0500

    Mixed Messages lesson

--- src/MixedMessages/test.java
+++ src/MixedMessages/test.java
@@ -1,10 +1,11 @@
-class Test {
+public class Test {
     public static void main (String [] args) {
         int x = 0;
         int y = 0;
         while ( x < 5 ) {
-            y = x - y;
-            System.out.print(x + "" + y +" ");
+            //y = x - y;
+            y = y + x;
+            System.out.println(x + "" + y +" ");
             x = x + 1;
         }
     }
